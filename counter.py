e9291d330932fbec2490ab6b4679728815780897
audreywl <audrey.lewis@students.olin.edu>
2016-03-14 01:56:08 -0400

    turning in minimum toolbox

--- counter.py
+++ counter.py
@@ -1,8 +1,8 @@
 """ A program that stores and updates a counter using a Python pickle file"""
 
-from os.path import exists
+import os.path
 import sys
-from pickle import dump, load
+import pickle
 
 def update_counter(file_name, reset=False):
 	""" Updates a counter stored in the file 'file_name'
@@ -18,7 +18,7 @@ def update_counter(file_name, reset=False):
 		reset: True if the counter in the file should be rest.
 		returns: the new counter value
 
-	>>> update_counter('blah.txt',True)
+	>>> update_counter('blah.txt')
 	1
 	>>> update_counter('blah.txt')
 	2
@@ -29,11 +29,21 @@ def update_counter(file_name, reset=False):
 	>>> update_counter('blah2.txt')
 	2
 	"""
-	pass
+	
+	if reset or not os.path.exists(file_name):
+		counter=1
+	else:
+		f = open(file_name,'r')
+		counter = pickle.load(f)
+		counter+=1
+	f = open(file_name,'w')
+	pickle.dump(counter,f)
+	return counter
+
 
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
 		import doctest
-		doctest.testmod()
+		doctest.testmod(verbose=True)
 	else:
 		print "new value is " + str(update_counter(sys.argv[1]))
